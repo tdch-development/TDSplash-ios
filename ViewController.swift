873e086cb259d3d32a283c2125e556577899db72
Sphinxx94 <brettmcfa@gmail.com>
2017-04-10 14:36:27 -0400

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TDSplash
+//
+//  Created by Swift on 2017-04-10.
+//  Copyright © 2017 TDCH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
